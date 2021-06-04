f812c98c39b88c758a066bd74b6863573b70f46c
ccz <ccz411@outlook.com>
2021-06-04 16:55:24 +0800

    1.添加mono单例模板

---
+++ Assets/Runtime/DesignPattern/MonoSingleTon.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace ZCCUtils.DesignPattern
+{
+    /// <summary>
+    /// 单例模板类
+    /// </summary>
+    public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        private static T instance;
+        public static T Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    GameObject obj = new GameObject();
+                    obj.name = typeof(T).Name + "<Singleton>";
+                    instance = obj.AddComponent<T>();
+                    DontDestroyOnLoad(obj);
+                }
+
+                return instance;
+            }
+        }
+    }
+}
