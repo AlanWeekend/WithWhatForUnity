4ccb864826077becadf890e9ae45e21f296a8739
ccz <ccz411@outlook.com>
2021-06-07 11:12:00 +0800

    1.修改单例模板

--- Assets/Runtime/DesignPattern/MonoSingleTon.cs
+++ Assets/Runtime/DesignPattern/MonoSingleTon.cs
@@ -7,21 +7,44 @@ namespace ZCCUtils.DesignPattern
     /// </summary>
     public class MonoSingleTon<T> : MonoBehaviour where T : MonoBehaviour
     {
-        private static T instance;
+        protected static T instance;
+
         public static T Instance
         {
             get
             {
                 if (instance == null)
                 {
-                    GameObject obj = new GameObject();
-                    obj.name = typeof(T).Name + "<Singleton>";
-                    instance = obj.AddComponent<T>();
-                    DontDestroyOnLoad(obj);
+                    // 如果不存在实例, 则查找所有这个类型的对象
+                    instance = FindObjectOfType(typeof(T)) as T;
+                    if (instance == null)
+                    {
+                        // 如果没有找到， 则新建一个
+                        GameObject obj = new GameObject(typeof(T).Name);
+                        // 对象不可见，不会被保存
+                        obj.hideFlags = HideFlags.HideAndDontSave;
+                        // 强制转换为 T 
+                        instance = obj.AddComponent(typeof(T)) as T;
+                    }
                 }
-
                 return instance;
             }
+        }
+
+        protected virtual void Awake()
+        {
+            if (instance == null)
+                instance = this as T;
+            else
+            {
+                GameObject.Destroy(instance);
+                instance = this as T;
+            }
+        }
+
+        private void OnDestroy()
+        {
+            instance = null;
         }
     }
 }
