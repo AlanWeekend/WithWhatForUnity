55d58f5777c650b13d1cd42e62a1d094509f19ae
ccz <ccz411@outlook.com>
2021-06-15 15:08:58 +0800

    1.添加transform类拓展，查找所有子物体不包含孙物体

---
+++ Runtime/ClassExtision/TransformExtision.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace ZCCUtils.ClassExtision
+{
+    public static class TransformExtision
+    {
+        /// <summary>
+        /// 获取所有的子物体，不包含孙物体
+        /// </summary>
+        /// <param name="transform"></param>
+        /// <returns></returns>
+        public static List<Transform> GetChilds(this Transform transform)
+        {
+            var childs = new List<Transform>();
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                childs.Add(transform.GetChild(i));
+            }
+            return childs;
+        }
+    }
+}
