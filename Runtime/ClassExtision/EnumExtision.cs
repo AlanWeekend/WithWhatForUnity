b86ebe83c1f11602418afb6db588e0a033c718e1
ccz <ccz411@outlook.com>
2021-06-16 17:59:41 +0800

    1.添加获取枚举所有名称的方法

---
+++ Runtime/ClassExtision/EnumExtision.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ZCCUtils.ClassExtision
+{
+    public static class EnumExtision
+    {
+        /// <summary>
+        /// 获取枚举所有名称
+        /// </summary>
+        /// <typeparam name="T">枚举类型</typeparam>
+        /// <param name="enum"></param>
+        /// <returns></returns>
+        public static List<string> GetEnumNamesList<T>(this T @enum) where T : Enum
+        {
+            return Enum.GetNames(typeof(T)).ToList();
+        }
+
+
+        /// <summary>
+        /// 获取枚举所有名称
+        /// </summary>
+        /// <param name="enumType">枚举类型typeof(T)</param>
+        /// <returns>枚举名称列表</returns>
+        public static List<string> GetEnumNamesList(this Type enumType)
+        {
+            return Enum.GetNames(enumType).ToList();
+        }
+    }
+}
