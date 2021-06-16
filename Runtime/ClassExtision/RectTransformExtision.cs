f614a7d59968e73919a941180902cd36f310c582
ccz <ccz411@outlook.com>
2021-06-16 11:31:59 +0800

    1.添加获取鼠标在ui上的位置函数

---
+++ Runtime/ClassExtision/RectTransformExtision.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace ZCCUtils.ClassExtision
+{
+    public static class RectTransformExtision
+    {
+        /// <summary>
+        /// 获取鼠标在UI上的位置 by lyb
+        /// </summary>
+        /// <param name="rectTransform">参照对象</param>
+        /// <param name="camera">相机,Screen Space - Camera 模式下需要传入画布指定的相机</param>
+        /// <returns></returns>
+        public static Vector3 GetMousePosition(this RectTransform rectTransform, Camera camera=null)
+        {
+            Vector2 pos = Vector2.zero;
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(rectTransform, Input.mousePosition, camera, out pos);
+            return new Vector3(pos.x, pos.y, 0);
+        }
+    }
+}
