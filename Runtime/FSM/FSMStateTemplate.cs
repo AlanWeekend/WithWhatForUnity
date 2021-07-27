ae77595994149f5d1770462eab9c1618caf7e336
zcc <872285171@qq.com>
2021-07-27 17:29:12 +0800

    1.添加fsm

---
+++ Runtime/FSM/FSMStateTemplate.cs
@@ -0,0 +1,16 @@
+﻿namespace ZCCUtils.FSM
+{
+    /// <summary>
+    /// 状态拥有者
+    /// </summary>
+    public class FSMStateTemplete<T> : FSMStateBase
+    {
+
+        public T owner;   //拥有者(范型)
+
+        public FSMStateTemplete(int id, T o) : base(id)
+        {
+            owner = o;
+        }
+    }
+}
