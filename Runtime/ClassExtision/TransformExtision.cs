051dba51f8bd23d519502daa73ee76494a7040ba
zcc <872285171@qq.com>
2021-08-18 20:42:50 +0800

    1.添加设置物体世界坐标为鼠标坐标的函数

--- Runtime/ClassExtision/TransformExtision.cs
+++ Runtime/ClassExtision/TransformExtision.cs
@@ -31,5 +31,22 @@ namespace ZCCUtils.ClassExtision
                 transform.GetChild(i).gameObject.SetActive(value);
             }
         }
+
+        /// <summary>
+        /// 设置世界坐标为鼠标所在位置
+        /// </summary>
+        /// <param name="transform"></param>
+        /// <param name="camera">相机</param>
+        /// <param name="targetGameObjectPosition">目标物体(参考物)</param>
+        /// <param name="offset">偏移量</param>
+        /// <returns>鼠标位置对应的世界坐标</returns>
+        public static void SetTheWorldPostionToTheMousePosition(this Transform transform, Camera camera, Vector3 targetGameObjectPosition,Vector3 offset)
+        {
+            //将对象坐标换成屏幕坐标
+            Vector3 pos = camera.WorldToScreenPoint(targetGameObjectPosition);
+            //让鼠标的屏幕坐标与对象坐标一致
+            Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, pos.z) + offset;
+            transform.position = camera.ScreenToWorldPoint(mousePos);
+        }
     }
 }
