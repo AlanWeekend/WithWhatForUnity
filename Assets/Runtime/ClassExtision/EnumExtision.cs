941d41811f2c753c7427d7f175fff7d2d099a9fd
zcc <872285171@qq.com>
2021-11-22 14:42:08 +0800

    0.2.0

--- Assets/Runtime/ClassExtision/EnumExtision.cs
+++ Assets/Runtime/ClassExtision/EnumExtision.cs
@@ -9,18 +9,6 @@ namespace ZCCUtils.ClassExtision
         /// <summary>
         /// 获取枚举所有名称
         /// </summary>
-        /// <typeparam name="T">枚举类型</typeparam>
-        /// <param name="enum"></param>
-        /// <returns></returns>
-        public static List<string> GetEnumNamesList<T>(this T @enum) where T : Enum
-        {
-            return Enum.GetNames(typeof(T)).ToList();
-        }
-
-
-        /// <summary>
-        /// 获取枚举所有名称
-        /// </summary>
         /// <param name="enumType">枚举类型typeof(T)</param>
         /// <returns>枚举名称列表</returns>
         public static List<string> GetEnumNamesList(this Type enumType)
